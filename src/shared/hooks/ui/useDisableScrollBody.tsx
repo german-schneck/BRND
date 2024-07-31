03db48ccd21364be494f26161198cfea5c4aaf3e
German D. Schneck <german.schneck@gmail.com>
2024-07-31 13:07:08 -0300

    fix(main): fix scroll trend brands

--- src/shared/hooks/ui/useDisableScrollBody.tsx
+++ src/shared/hooks/ui/useDisableScrollBody.tsx
@@ -2,12 +2,28 @@ import { useEffect } from 'react';
 
 const useDisableScrollBody = () => {
   useEffect(() => {
+    // Store the original overflow style
+    const originalStyle = window.getComputedStyle(document.body).overflow;
+    
     // Set overflow to hidden when component mounts
     document.body.style.overflow = 'hidden';
 
-    // Reset overflow to auto when component unmounts
+    // Reset overflow to its original value when component unmounts
     return () => {
-      document.body.style.overflow = 'auto';
+      document.body.style.overflow = originalStyle;
+    };
+  }, []);
+
+  // Add a resize event listener to ensure the style is applied even after window resizing
+  useEffect(() => {
+    const handleResize = () => {
+      document.body.style.overflow = 'hidden';
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
     };
   }, []);
 };
